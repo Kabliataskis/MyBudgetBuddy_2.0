3beac3af734762ead729bbd9614b74de9510607c
gagerma <slavikbanana87@gmail.com>
2023-03-31 18:31:27 +0100

    Vel pakeisti failai

---
+++ my_budget_buddy/src/Puslapiai/Pagrindinis_puslapis/Pagrindinis_puslapis.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import './Pagrindinis_puslapis_dizainas.css';
+
+function Pirmas_puslapis () {
+	return (
+		<div className='Bendra_informacija'>
+			<div className='Informacija'>
+				<div className='Informacios_skaiciai'> 
+					<h2> Biudžietas</h2>
+				</div>
+				<div className='Informacios_skaiciai'> 
+					<h2> Išleista per menesi</h2>
+				</div>
+				<div>
+					
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Pirmas_puslapis;
