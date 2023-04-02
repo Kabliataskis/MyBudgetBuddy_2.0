5b6fe0656a17f18382420f7b9c5b1db434f250f6
gagerma <slavikbanana87@gmail.com>
2023-04-02 15:06:47 +0100

    Padaritas skriptas leiciantis naikit pajamas

--- my_budget_buddy/src/Puslapiai/Pajamos/Pajamos.js
+++ my_budget_buddy/src/Puslapiai/Pajamos/Pajamos.js
@@ -1,11 +1,39 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import './Pajamos_dizainas.css';
+import swal from 'sweetalert2'
 
 function Pajamos () {
+	function delete_pajamas (id) {
+		swal.fire({
+			title: "Veiksmo patvirtinimas",
+			text: "Ar tikrai norite ištrinti įrašą?",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: '#d33',
+			cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Ištrinti',
+            cancelButtonText: "Atšaukti!"
+		  }).then((result) => {
+			if (result.isConfirmed) {
+				// užklausa į backend
+
+				// success
+				// swal.fire(
+				//   'Deleted!',
+				//   'Your file has been deleted.',
+				//   'success'
+				// )
+			  }
+		  });
+
+	}
 	return (
-		<div>Pajamu_puslapis</div>
+		<div>
+			<button onClick={()=>delete_pajamas(1)}>Delete pajama</button>
+		</div>
 	);
 }
 
 export default Pajamos;
+
