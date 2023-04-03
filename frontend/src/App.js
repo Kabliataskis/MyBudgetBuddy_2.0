75726513cea77e374b575b1c66baa883b6684f1c
Daniel <danielpsev@gmail.com>
2023-04-03 08:40:32 +0300

    Suvesti pajamas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import './App.css';
+import PajamosSuvesti from './Puslapiai/Pajamos/PajamosSuvesti';
+
+
 
 function App() {
   return (
     <div className="App">
-    
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+      <PajamosSuvesti/>
     </div>
   );
 }
