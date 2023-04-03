d07f387bb3d5c4c021b5e8e7add82edac6c14352
danielpsev <danielpsev@gmail.com>
2023-04-03 10:37:23 +0300

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import './App.css';
-import PajamosSuvesti from './Puslapiai/Pajamos/PajamosSuvesti';
-
-
+import "./App.css";
+import PajamosSuvesti from "./Puslapiai/Pajamos/PajamosSuvesti";
 
 function App() {
   return (
     <div className="App">
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="colored"
-    />
-      <PajamosSuvesti/>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <PajamosSuvesti />
     </div>
   );
 }
--- frontend/src/Puslapiai/Pajamos/PajamosSuvesti.js
+++ frontend/src/Puslapiai/Pajamos/PajamosSuvesti.js
@@ -1,88 +1,132 @@
 /* eslint-disable linebreak-style */
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { toast } from "react-toastify";
-import {AiOutlineClose} from 'react-icons/ai'
-import './Pajamos_dizainas.css';
+import { AiOutlineClose } from "react-icons/ai";
+import "./Pajamos_dizainas.css";
 
-function PajamosSuvesti () {
-    const max_amount = 9999999 // Maksimali suma €
-    const [modal_PajamosSuvesti, setModal_PajamosSuvesti] = useState(false);
+function PajamosSuvesti() {
+  const max_amount = 9999999; // Maksimali suma €
+  const [modal_PajamosSuvesti, setModal_PajamosSuvesti] = useState(false);
 
-    const defaultInputValues = {
-        title: '',
-        date: '',
-        amount: ''
+  const defaultInputValues = {
+    title: "",
+    date: "",
+    amount: "",
+  };
+  const [formInputs, setFormInputs] = useState(defaultInputValues);
+  const { title, date, amount } = formInputs;
+
+  // Modal close on background click
+  const onMouseDown = (e) => {
+    if (e.target === e.currentTarget) {
+      closeModal();
     }
-    const [formInputs, setFormInputs] = useState(defaultInputValues);
-    const {title, date, amount} = formInputs;
+  };
+  // Modal close and clear input's
+  const closeModal = () => {
+    setModal_PajamosSuvesti(false);
+    setFormInputs(defaultInputValues);
+  };
 
-    // Modal close on background click
-    const onMouseDown = (e) => {
-        if (e.target === e.currentTarget) {
-          closeModal();
-        }
-      };
-    // Modal close and clear input's
-    const closeModal = () => {
-        setModal_PajamosSuvesti(false);
-        setFormInputs(defaultInputValues);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const val = e.target.value;
+    setFormInputs({ ...formInputs, [name]: val });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title && date && amount) {
+      let selected_time = new Date(date).getTime();
+      let curr_time = new Date().getTime();
+
+      if (title.length > 20) {
+        toast.error("Pavadinimo ilgis iki 20 simbolių!");
+      } else if (selected_time > curr_time) {
+        toast.error("Data negali būti didesnė už dabartinį laiką");
+      } else if (amount > max_amount) {
+        toast.error(`Suma negali viršyti ${max_amount} €`);
+      } else {
+        // Frontend validation success
+        // Request to backend
+      }
+    } else {
+      toast.error("Užpildykite visus laukelius!");
     }
+  };
+  return (
+    <>
+      <button onClick={() => setModal_PajamosSuvesti(true)}>
+        Pridėti pajamas
+      </button>
+      {modal_PajamosSuvesti ? (
+        <div className="Pajamos-modal-container" onMouseDown={onMouseDown}>
+          <div className="modal-content">
+            <h2 className="modal-title">Pridėti pajamas</h2>
+            <span className="modal-close-btn" onClick={() => closeModal()}>
+              <AiOutlineClose />
+            </span>
+            <form className="Pajamos-modal-form" onSubmit={handleSubmit}>
+              <label htmlFor="title">Pavadinimas</label>
+              <p>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  placeholder="Pavadinimas"
+                  maxLength="20"
+                  required
+                  onChange={handleChange}
+                  value={title}
+                />
+              </p>
 
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const val = e.target.value;
-        setFormInputs({...formInputs, [name]: val});
-    }
+              <label htmlFor="date">Data</label>
+              <p>
+                <input
+                  type="datetime-local"
+                  name="date"
+                  id="date"
+                  required
+                  onChange={handleChange}
+                  value={date}
+                />
+              </p>
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+              <label htmlFor="title">Suma</label>
+              <p className="eur">
+                <input
+                  type="number"
+                  name="amount"
+                  id="amount"
+                  placeholder="Suma"
+                  min="0.01"
+                  max={max_amount}
+                  step="0.01"
+                  autoComplete="off"
+                  required
+                  onChange={handleChange}
+                  value={amount}
+                />
+              </p>
 
-        if(title && date && amount){
-            let selected_time = new Date(date).getTime()
-            let curr_time = new Date().getTime();
-
-            if(title.length > 20){
-                toast.error("Pavadinimo ilgis iki 20 simbolių!");
-            }else if(selected_time > curr_time){
-                toast.error("Data negali būti didesnė už dabartinį laiką");
-            }else if(amount > max_amount){
-                toast.error(`Suma negali viršyti ${max_amount} €`);
-            }else{
-                // Frontend validation success
-                // Request to backend
-            }
-
-        }else{
-            toast.error('Užpildykite visus laukelius!');
-        }
-    }
-s
-	return (
-        <>
-            <button onClick={() => setModal_PajamosSuvesti(true)} >Pridėti pajamas</button>
-            {modal_PajamosSuvesti ?             
-            <div className="Pajamos-modal-container" onMouseDown={onMouseDown}>
-                <div className="modal-content">
-                        <h2 className="modal-title">Pridėti pajamas</h2>
-                        <span className="modal-close-btn" onClick={() => closeModal()}><AiOutlineClose/></span>
-                    <form className="Pajamos-modal-form" onSubmit={handleSubmit}>
-                        <label htmlFor="title">Pavadinimas</label>
-                        <p><input type="text" name="title" id="title" placeholder="Pavadinimas" maxLength="20"  required onChange={handleChange} value={title}/></p>
-
-                        <label htmlFor="date">Data</label>
-                        <p><input type="datetime-local" name='date' id="date" required onChange={handleChange} value={date}/></p>
-                        
-                        <label htmlFor="title">Suma</label>
-                        <p className='eur'><input type="number" name="amount" id="amount" placeholder="Suma" min="0.01" max={max_amount} step="0.01" autoComplete='off' required onChange={handleChange} value={amount}/></p>
-
-                        <button className="add-btn" type="submit">Pridėti</button>
-                        <button className="cancel-btn" type="reset" onClick={() => closeModal()} >Atšaukti</button>
-                    </form>
-                </div>
-            </div>
-            : null}
-        </>
-	);
+              <button className="add-btn" type="submit">
+                Pridėti
+              </button>
+              <button
+                className="cancel-btn"
+                type="reset"
+                onClick={() => closeModal()}
+              >
+                Atšaukti
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 }
 
 export default PajamosSuvesti;
