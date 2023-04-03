a314157438f8d7421f2798521c2dc41dcbbbc96b
nikita23421 <113423121+nikita23421@users.noreply.github.com>
2023-04-03 14:55:02 +0300

    Update Pagrindinis_puslapis.js

--- frontend/src/Puslapiai/Pagrindinis_puslapis/Pagrindinis_puslapis.js
+++ frontend/src/Puslapiai/Pagrindinis_puslapis/Pagrindinis_puslapis.js
@@ -7,10 +7,18 @@ function Pirmas_puslapis () {
 		<div className='Bendra_informacija'>
 			<div className='Informacija'>
 				<div className='Informacios_skaiciai'> 
-					<h2> Biudžietas</h2>
+					<h2> Biudžietas</h2><div className='Pagrindinis-container'>
+                    <div className="biudzetas">Biudzetas</div>
+       <div className='isleista-per-men'>Isleista per men</div>
+       <div className='Pagrindinis-tables-container'>
+          <div className="islaidu_table">islaidu table</div>
+          <div className= "ivesti_pajomos">ivesti pajomos</div>
+        </div>
+      </div>
 				</div>
 				<div className='Informacios_skaiciai'> 
 					<h2> Išleista per menesi</h2>
+					<div className='isleista-per-men'>Isleista per men</div>
 				</div>
 				<div>
 					
