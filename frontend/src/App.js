fcdf61c10618b5638346be8971b1614bcbcc663a
nikita23421 <113423121+nikita23421@users.noreply.github.com>
2023-04-04 10:27:00 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Pirmas_puslapis from './Puslapiai/Pagrindinis_puslapis/Pagrindinis_puslapis';
 
 function App() {
   return (
     <div className="App">
-      
+      <Pirmas_puslapis/>
     </div>
   );
 }
