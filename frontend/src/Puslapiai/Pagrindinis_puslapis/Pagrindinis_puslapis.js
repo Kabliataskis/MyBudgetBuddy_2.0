9e0c2d515438f6b50737a6c623531ea7369944c1
nikita23421 <113423121+nikita23421@users.noreply.github.com>
2023-04-04 10:53:43 +0300

    .

--- frontend/src/Puslapiai/Pagrindinis_puslapis/Pagrindinis_puslapis.js
+++ frontend/src/Puslapiai/Pagrindinis_puslapis/Pagrindinis_puslapis.js
@@ -6,7 +6,7 @@ function Pirmas_puslapis () {
 	return (
 		<div className='Bendra_informacija'>
 			<div className='Informacija'>
-				<div className='Informacios_skaiciai'> 
+				
 					<h2> Biudžietas</h2><div className='Pagrindinis-container'>
                     <div className="biudzetas">Biudzetas</div>
        <div className='isleista-per-men'>Isleista per men</div>
@@ -14,15 +14,9 @@ function Pirmas_puslapis () {
           <div className="islaidu_table">islaidu table</div>
           <div className= "ivesti_pajomos">ivesti pajomos</div>
         </div>
-      </div>
-				</div>
-				<div className='Informacios_skaiciai'> 
+      </div> 
 					<h2> Išleista per menesi</h2>
 					<div className='isleista-per-men'>Isleista per men</div>
-				</div>
-				<div>
-					
-				</div>
 			</div>
 		</div>
 	);
