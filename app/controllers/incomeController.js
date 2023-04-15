0841b413725474f445a4213054f364cf2d304137
Daniel <danielpsev@gmail.com>
2023-04-15 20:45:11 +0300

    .

--- app/controllers/incomeController.js
+++ app/controllers/incomeController.js
@@ -1,10 +1,16 @@
-const income = require("../models/incomeModel");
+const Income = require("../models/incomeModel");
 
 exports.addIncome = async (req, res) => {
     try{
-
+        const newIncome = await Income.create({
+            user_id: 1,
+            title: req.body.title,
+            sum: req.body.sum,
+            date: req.body.date
+        });
+        res.status(200).json({status: "success", mess: "Įrašas pridėtas sėkmingai!"});
     } catch (err){
-
+        res.status(500).json({mess: err});
     }
 };
 exports.deleteIncome = async (req, res) => {
