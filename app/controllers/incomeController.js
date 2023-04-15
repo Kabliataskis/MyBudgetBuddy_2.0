6d5379133fbc50cc42dbd1d667f2211b670fc6d7
Daniel <danielpsev@gmail.com>
2023-04-15 21:41:20 +0300

    Pajamu ivedimas

--- app/controllers/incomeController.js
+++ app/controllers/incomeController.js
@@ -1,6 +1,7 @@
 const Income = require("../models/incomeModel");
 
 exports.addIncome = async (req, res) => {
+    console.log("new income request");
     try{
         const newIncome = await Income.create({
             user_id: 1,
@@ -8,11 +9,14 @@ exports.addIncome = async (req, res) => {
             sum: req.body.sum,
             date: req.body.date
         });
+        console.log(newIncome);
         res.status(200).json({status: "success", mess: "Įrašas pridėtas sėkmingai!"});
     } catch (err){
         res.status(500).json({mess: err});
     }
 };
+
+
 exports.deleteIncome = async (req, res) => {
     try{
 
