db4f2edc000c19682b81e12f28dc2e16b04adaf8
Inga <Inga.Stankuviene@stud.vtmc.lt>
2023-05-17 23:13:20 +0300

    Start automation test

---
+++ app/src/main/java/AccountRegister.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountRegister extends PageObject {
+
+    public AccountRegister(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
---
+++ app/src/test/java/AccountRegisterTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountRegisterTest extends BaseTest{
+    @Test
+
+    void register(){
+        AccountRegister accountRegister = new AccountRegister(driver);
+
+
+
+    }
+
+}
