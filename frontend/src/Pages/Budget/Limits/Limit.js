ca2f643cbd5931e880650e5240c7e36cb6b987aa
danielpsev <danielpsev@gmail.com>
2023-05-18 13:09:01 +0300

    .

--- frontend/src/Pages/Budget/Limits/Limit.js
+++ frontend/src/Pages/Budget/Limits/Limit.js
@@ -30,10 +30,13 @@ export default function Limit(props) {
     }));
   };
 
-  const[limitWidth, setLimitWidth] = useState(); 
-  const[spentWidth, setSpentWidth] = useState();
+  const [limitWidth, setLimitWidth] = useState();
+  const [spentWidth, setSpentWidth] = useState();
   useEffect(() => {
-    if (spent === 0) {
+    if (spent === 0 && limit === 0) {
+      setLimitWidth("50%");
+      setSpentWidth("50%");
+    } else if (spent === 0) {
       setLimitWidth("100%");
       setSpentWidth("0%");
     } else {
@@ -43,20 +46,28 @@ export default function Limit(props) {
     console.log("effect");
   }, [limit, spent]);
 
-
-
   return (
     <div className="budget-limit-container">
       <img src={category.imgSrc} className="budget-limit__category-icon" />
       <h3>{category.title}</h3>
       <div className="budget-linechart">
-        <div className="horizontal-bar__pelnas" style={{width: limitWidth}}>{limit} €</div>
-        <div className="horizontal-bar__islaidos" style={{width: spentWidth}}>{spent} €</div>
+        <div className="horizontal-bar__pelnas" style={{ width: limitWidth }}>
+          {limit} €
+        </div>
+        <div className="horizontal-bar__islaidos" style={{ width: spentWidth }}>
+          {spent} €
+        </div>
       </div>
       <div className="budget-limit-info">
         <h4>
-           Nustatytas biudžetas: <span className="green"> {currLimit.amount} €</span>
-            <span className="red budget-limit__editBtn" onClick={() => editLimit(_id)}> <FaPen /> </span>
+          Nustatytas biudžetas:
+          <span className="green"> {currLimit.amount} €</span>
+          <span
+            className="red budget-limit__editBtn"
+            onClick={() => editLimit(_id)}
+          >
+            <FaPen />
+          </span>
         </h4>
 
         <h4>
