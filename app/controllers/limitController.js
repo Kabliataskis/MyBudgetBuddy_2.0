9479db93676cc37102642e34d978fc123e3156d3
danielpsev <danielpsev@gmail.com>
2023-05-18 15:08:25 +0300

    .

--- app/controllers/limitController.js
+++ app/controllers/limitController.js
@@ -5,58 +5,72 @@ const Expense = require("../models/expenseModel");
 const { getCategory } = require("./categoryController");
 
 exports.getAllLimits = async (req, res) => {
-  const {year, month} = req.params;
-  console.log("year:"+ req.params.year);
-  console.log("month:"+ req.params.month);
+  const { year, month } = req.params;
+  console.log("year:" + req.params.year);
+  console.log("month:" + req.params.month);
 
-
-  if(!year || !month || month > 12 || month < 0 || year < 2022){
-    res.status(400).json({status: "error", msg: 'Blogai nurodyti metai arba mėnesis',
-    });
-  }else{
+  if (!year || !month || month > 12 || month < 0 || year < 2022) {
+    res
+      .status(400)
+      .json({ status: "error", msg: "Blogai nurodyti metai arba mėnesis" });
+  } else {
     let category_spent = [];
     try {
-    
+      //2022-05
+      //2023-05
       const selectedDate = new Date(year, month - 1, 1);
-      const startOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
-      const endOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0);
-  
-  
+      console.log(`selcted date: ${selectedDate}`);
+      const startOfMonth = new Date(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth(),
+        1
+      );
+      const endOfMonth = new Date(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth() + 1,
+        0
+      );
+
       console.log(`The year is ${year} the month is ${month}`);
       console.log(`startOfMonth ${startOfMonth}`);
       console.log(`endOfMonth ${endOfMonth}`);
-  
-      allLimits = await Limit.find({ user_id: req.userInfo.id,   date: { $gte: startOfMonth, $lte: endOfMonth }, }).populate({
+
+      allLimits = await Limit.find({
+        user_id: req.userInfo.id,
+        date: { $gte: startOfMonth, $lte: endOfMonth },
+      }).populate({
         path: "category",
         model: "category",
       });
       const GetCategory = await Category.find();
-      if ((!allLimits.length && GetCategory) || (allLimits.length > 0 && GetCategory && allLimits.length != GetCategory.length)) {
-        const existingCategories = allLimits.map((limit) => limit.category.title);
+      if (
+        (!allLimits.length && GetCategory) ||
+        (allLimits.length > 0 &&
+          GetCategory &&
+          allLimits.length != GetCategory.length)
+      ) {
+        const existingCategories = allLimits.map(
+          (limit) => limit.category.title
+        );
         for (const category of GetCategory) {
           if (!existingCategories.includes(category.title)) {
-              console.log('create new' + category.title);
+            console.log("create new" + category.title);
 
-              const limit = await Limit.create({
-                user_id: req.userInfo.id,
-                category: category._id,
-                limit: 0,
-                date: startOfMonth
-              });
-              console.log(limit);
-              allLimits.push(limit);
-            }else{
-              console.log("no creation");
-            }
+            const limit = await Limit.create({
+              user_id: req.userInfo.id,
+              category: category._id,
+              limit: 0,
+              date: startOfMonth,
+            });
+            console.log(limit);
+            allLimits.push(limit);
+          } else {
+            console.log("no creation");
+          }
         }
-
       } else {
         //no category
       }
-  
-  
-
-
 
       for (const category of GetCategory) {
         const sum = await Expense.aggregate([
@@ -64,7 +78,10 @@ exports.getAllLimits = async (req, res) => {
             $match: {
               user_id: req.userInfo.id,
               category: new ObjectId(category._id),
-              date: { $gte: new Date(startOfMonth), $lte: new Date(endOfMonth) }
+              date: {
+                $gte: new Date(startOfMonth),
+                $lte: new Date(endOfMonth),
+              },
             },
           },
           {
@@ -81,12 +98,15 @@ exports.getAllLimits = async (req, res) => {
           },
         ]);
         if (sum && sum.length > 0 && sum[0].total) {
-          category_spent.push({ category: category.title, spent: sum[0].total });
+          category_spent.push({
+            category: category.title,
+            spent: sum[0].total,
+          });
         } else {
           category_spent.push({ category: category.title, spent: 0 });
         }
       }
-  
+
       res.status(200).json({
         status: "success",
         results: allLimits.length,
@@ -102,15 +122,7 @@ exports.getAllLimits = async (req, res) => {
       });
     }
   }
-
-
 };
-
-
-
-
-
-
 
 // exports.getAllLimits = async (req, res) => {
 //   const { limit = 0 } = req.query;
@@ -126,16 +138,12 @@ exports.getAllLimits = async (req, res) => {
 //     //   .sort({ createdAt: -1 })
 //     //   .limit(limit);
 
-
-
 //     const startOfMonth = new Date(year, month - 1, 1);
 //     const endOfMonth = new Date(year, month, 1, 0, 0, -1);
 
 //     console.log(`The year is ${year} the month is ${month}`);
 //     console.log(`startOfMonth ${startOfMonth}`);
 //     console.log(`endOfMonth ${endOfMonth}`);
-
-
 
 //     allLimits = await Limit.find({ user_id: req.userInfo.id }).populate({
 //       path: "category",
@@ -197,10 +205,6 @@ exports.getAllLimits = async (req, res) => {
 //     });
 //   }
 // };
-
-
-
-
 
 exports.getLimit = async (req, res) => {
   try {
