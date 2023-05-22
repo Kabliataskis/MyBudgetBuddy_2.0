990ba540e5749bbf4a498e15e76298cc08f90e56
Vyciok <113423064+Vyciok@users.noreply.github.com>
2023-05-22 22:28:36 +0300

    beta

--- frontend/src/Pages/Budget/Charts/Multiaxis_Line_Chart.js
+++ frontend/src/Pages/Budget/Charts/Multiaxis_Line_Chart.js
@@ -58,7 +58,7 @@ export default function MultiAxgis(props) {
       },
       y1: {
         type: "linear",
-        display: true,
+        display: false,
         position: "right",
         grid: {
           drawOnChartArea: false,
@@ -80,7 +80,7 @@ export default function MultiAxgis(props) {
       data: data2,
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      yAxisID: 'y1',
+      yAxisID: 'y',
     },
     {
       label: 'Pajamos',
