034e81656baf96b1abe469cde54fa3573761de47
Daniel <danielpsev@gmail.com>
2023-05-23 13:55:26 +0300

    .

--- frontend/src/Pages/Budget/Charts/Multiaxis_Line_Chart.js
+++ frontend/src/Pages/Budget/Charts/Multiaxis_Line_Chart.js
@@ -21,27 +21,8 @@ ChartJS.register(
   Legend
 );
 
-
 export default function MultiAxgis(props) {
-  const {expenses_months,expenses_spent,incomes_earned} = props;
-//   const [startDate, setStartDate] = useState("");
-//   const [endDate, setEndDate] = useState("");
-
-//   const filterIncome = incomes.filter((el) => {
-//     const date = el.date || "";
-  
-//     const startDateObj = startDate ? new Date(startDate) : null;
-//     const endDateObj = endDate ? new Date(endDate) : null;
-//     const incomeDateObj = date ? new Date(date) : null;
-
-//     const dateInRange =
-//       (!startDateObj ||
-//         startDateObj <= incomeDateObj.setHours(0, 0, 0, 0) + 86400000) &&
-//       (!endDateObj || endDateObj >= incomeDateObj.setHours(0, 0, 0, 0));
-
-//     return dateInRange;
-//   });
-// console.log(filterIncome);
+  const { expenses_months, expenses_spent, incomes_earned } = props;
 
   const options = {
     responsive: true,
@@ -66,52 +47,30 @@ export default function MultiAxgis(props) {
       },
     },
   };
-  
 
   const labels = expenses_months;
   const data1 = incomes_earned;
   const data2 = expenses_spent;
 
- const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Išlaidos',
-      data: data2,
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      yAxisID: 'y1',
-    },
-    {
-      label: 'Pajamos',
-      data: data1,
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      yAxisID: 'y',
-    }
-    
-  ],
-};
-
-  // const data = {
-  //   labels,
-  //   datasets: [
-  //     {
-  //       label: "Pajamos",
-  //       data: data1,
-  //       borderColor: "rgb(255, 99, 132)",
-  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
-  //       yAxisID: "y",
-  //     },
-  //     {
-  //       label: "Islaidos",
-  //       data: data2,
-  //       borderColor: "rgb(53, 162, 235)",
-  //       backgroundColor: "rgba(53, 162, 235, 0.5)",
-  //       yAxisID: "y1",
-  //     },
-  //   ],
-  // };
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Pajamos",
+        data: data1,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        yAxisID: "y1",
+      },
+      {
+        label: "Išlaidos",
+        data: data2,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        yAxisID: "y",
+      },
+    ],
+  };
 
   return <Line options={options} data={data} />;
 }
