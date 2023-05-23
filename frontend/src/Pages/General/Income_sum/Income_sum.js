58f201962555ca43faf4724d16bab2912c21ac5d
Daniel <danielpsev@gmail.com>
2023-05-23 22:09:08 +0300

    KB-130 fix (pajamu/islaidu skaiciavimas)

--- frontend/src/Pages/General/Income_sum/Income_sum.js
+++ frontend/src/Pages/General/Income_sum/Income_sum.js
@@ -4,12 +4,21 @@ export default function calculateTotalIncome(incomes) {
   }
 
   // Filtruojame pajamas pagal esamą mėnesį
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth() + 1; // Pridedame 1, nes mėnuo numeruojamas nuo 0 iki 11
+  // const currentDate = new Date();
+  // const currentMonth = currentDate.getMonth() + 1; // Pridedame 1, nes mėnuo numeruojamas nuo 0 iki 11
   const filteredIncomes = incomes.filter(income => {
+    // const incomeDate = new Date(income.date);
+    // const incomeMonth = incomeDate.getMonth() + 1;
+    // return incomeMonth === currentMonth;
     const incomeDate = new Date(income.date);
-    const incomeMonth = incomeDate.getMonth() + 1;
-    return incomeMonth === currentMonth;
+    const incomeYear = incomeDate.getFullYear();
+    const incomeMonth = incomeDate.getMonth();
+  
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+  
+    return incomeYear === currentYear && incomeMonth === currentMonth;
   });
   const totalIncome = filteredIncomes.reduce((total, income) => total + income.sum, 0);
   localStorage.setItem("totalIncomeCurrMonth", totalIncome);
